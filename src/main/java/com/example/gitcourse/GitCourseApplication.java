f60ffc233db67160056b35c0c713d0934ae02f07
nicolas <nicolas.mine11@gmail.com>
2023-03-19 16:16:52 +0100

    first commit

---
+++ src/main/java/com/example/gitcourse/GitCourseApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCourseApplication {
+
+    public static void main(String[] args) {
+        //Test2
+        SpringApplication.run(GitCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitcourse/GitCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
