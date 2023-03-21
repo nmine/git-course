8b19b1230bcc29fdb76f211fc5c92834c0fc9cbe
nicolas <nicolas.mine11@gmail.com>
2023-03-21 12:55:18 +0100

    commit8

--- src/main/java/com/example/gitcourse/GitCourseApplication.java
+++ src/main/java/com/example/gitcourse/GitCourseApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitCourseApplication {
 
     public static void main(String[] args) {
-        //Test2
-        //
-        SpringApplication.run(GitCourseApplication.class, args);
+
     }
     //fix commit 3pp
 
